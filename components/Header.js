f75c3d61489dbde05994c2eeb1a665dc7fa8336d
onlinesuksesabadi <feyberho@gmail.com>
2021-10-12 18:57:33 +0700

    remove darkmode

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,7 @@
 import Link from 'next/link'
-import useDarkMode from "./useDarkMode";
 import Image from 'next/image';
 
 export default function Header() {
-  const [colorTheme, setTheme] = useDarkMode();
     return (
     <header>
         <div className=' flex justify-center items-center'>
